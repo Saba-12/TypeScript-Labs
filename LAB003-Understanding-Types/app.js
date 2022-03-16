56cfdeada778fff5cfd1b267b5381b0ea8f5da4a
Saba-12 <101691122+Saba-12@users.noreply.github.com>
2022-03-16 22:34:31 +0530

    Lab-3 done

---
+++ LAB003-Understanding-Types/app.js
@@ -0,0 +1,5 @@
+function totalPrice(n1, n2) {
+    return n1 + n2;
+}
+var result = totalPrice(50, 15.6);
+console.log("Total price of the order is : " + result);
---
+++ LAB003-Understanding-Types/app.ts
@@ -0,0 +1,7 @@
+function totalPrice(n1:number,n2:number){
+    return n1+n2;
+}
+
+let result=totalPrice(50,15.6);
+
+console.log("Total price of the order is : "+result);
